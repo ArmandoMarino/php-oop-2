e072c8523f567ac7c338f45e4081728f3c199d62
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-20 16:09:38 +0100

    ex category

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-
 // - L'e-commerce vende prodotti per animali.
 // - I prodotti sono categorizzati, le categorie sono Cani o Gatti.
 // - Tra i prodotti, troviamo cibo, giochi, cucce, etc.
+
+
+class Category
+{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
 
 
 class Product
@@ -10,19 +20,25 @@ class Product
     public $id;
     public $title;
     public $price;
+    public $category;
 
-    public function __construct($id, $title, $price)
+    public function __construct($id, $title, $price, Category $category)
     {
         $this->id = $id;
         $this->title = $title;
         $this->price = $price;
+        $this->category = $category;
     }
 
     public function getDetails()
     {
-        return "$this->title $this->price ";
+        $category_name = $this->category->name;
+        return "Prodotto : $this->title Euro : $this->price  Categoria : $category_name";
     }
 }
 
 
-$Ultima = new Product(1, 'Ultima Cibo per Cani', 36);
+$cani = new Category('cani');
+
+$Ultima = new Product(1, 'Ultima Cibo per Cani', 36, $cani);
+var_dump($Ultima);
